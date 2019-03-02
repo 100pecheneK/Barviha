0c5f55616427002295968cb633fab606e9d1d9cf
iwenzelw <47976290+iwenzelw@users.noreply.github.com>
2019-03-02 10:53:21 +0500

    добавил колонки

--- test/WENZEL/the_main/admin.py
+++ test/WENZEL/the_main/admin.py
@@ -1,7 +1,19 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(Subscribers)
+
+
+class SubscribersAdmin (admin.ModelAdmin):
+    list_display = ("first_name", "last_name",) #"email" список
+    list_filter = ("first_name",) # фильтр по имени 
+    #list_display = (field.name for field in Subscribers._meta.fields)
+    fields = ("email",) # показывать только email
+    #exclude = ("email",)
+    search_fields = ("first_name", "email") #поиск по ...
+    class Meta:
+        model = Subscribers
+
+admin.site.register(Subscribers, SubscribersAdmin)
 admin.site.register(apartment)
 admin.site.register(rental)
 
--- test/WENZEL/the_main/models.py
+++ test/WENZEL/the_main/models.py
@@ -14,6 +14,15 @@ class Subscribers(models.Model):
         max_length = 200,
         blank = True,
     )
+        def __str__(self):
+          return "Агент %s %s" % (self.first_name, self.email)
+        
+        class Meta:
+            db_table = ''
+            managed = True
+            verbose_name = 'Риелтора'
+            verbose_name_plural = 'Риелторы'
+
 class apartment(models.Model):
     title = models.CharField(
         max_length=200,
@@ -24,6 +33,12 @@ class apartment(models.Model):
         max_length=200,
         blank=True,
     )
+    class Meta:
+            db_table = ''
+            managed = True
+            verbose_name = 'Квартиру'
+            verbose_name_plural = 'Квартиры'
+    
 
 class rental(models.Model):
     title = models.CharField(
@@ -35,3 +50,8 @@ class rental(models.Model):
         max_length=200,
         blank=True,
     )
+    class Meta:
+            db_table = ''
+            managed = True
+            verbose_name = 'Арену квартиры'
+            verbose_name_plural = 'Аренда квартиры'
