15edcc746a80d21155ee3d9640ff38c497eb7922
iwenzelw <47976290+iwenzelw@users.noreply.github.com>
2019-03-02 17:38:05 +0500

    формы регистрации

--- test/WENZEL/WENZEL/settings.py
+++ test/WENZEL/WENZEL/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -122,3 +123,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
--- test/WENZEL/users/views.py
+++ test/WENZEL/users/views.py
@@ -1,16 +1,15 @@
 from django.shortcuts import render,redirect
-from django.contrib.auth.forms import UserCreationForm
+from .forms import UserRegistration
 from django.contrib import messages
 
 def register(request):
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        form = UserRegistration(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Пользователь {username} был успешно создан!')
             return redirect('the_main-home')
-            
     else:
-        form = UserCreationForm()
+        form = UserRegistration()
     return render(request, 'users/registration.html', {'form': form, 'title': 'Регистрация пользователя'})
