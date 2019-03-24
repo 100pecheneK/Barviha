43aa71af40792d21bcf5625d095ca45d7e2ac399
iwenzelw <47976290+iwenzelw@users.noreply.github.com>
2019-03-24 12:25:08 +0500

    перевел на русский

--- prosite/adminsite/admin.py
+++ prosite/adminsite/admin.py
@@ -1,13 +1,20 @@
 from django.contrib import admin
-from .models import Room
+from .models import Room, Rent, District
 
 admin.site.site_header = 'wenzel administration'
 
 
 # Register your models here.
 class RoomAdmin(admin.ModelAdmin):
-	list_display = ('name',  'price', 'old_price', 'created_at', 'updated_at',)
+	list_display = ('name', 'price', 'old_price', 'created_at', 'updated_at',)
 	list_filter = ('name', 'created_at', 'price',)
 
 
+class RentAdmin(admin.ModelAdmin):
+	list_display = ('name', 'price', 'old_price', 'created_at', 'updated_at',)
+	list_filter = ('name', 'created_at', 'price',)
+
+
+admin.site.register(Rent, RentAdmin)
 admin.site.register(Room, RoomAdmin)
+admin.site.register(District)
--- prosite/adminsite/apps.py
+++ prosite/adminsite/apps.py
@@ -3,3 +3,4 @@ from django.apps import AppConfig
 
 class AdminsiteConfig(AppConfig):
     name = 'adminsite'
+    verbose_name = 'Административный отдел'
